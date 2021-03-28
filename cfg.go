8bb9bef3c909a7834b3358ee8728d3706f796390
Gabriel Handford <gabrielh@gmail.com>
2021-03-28 12:14:32 -0700

    Config

--- cfg.go
+++ cfg.go
@@ -4,9 +4,65 @@ import (
 	"database/sql"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/keys-pub/keys"
 	"github.com/keys-pub/keys/encoding"
 	"github.com/pkg/errors"
 )
+
+type Config struct {
+	db *sqlx.DB
+}
+
+func (v *Vault) Config() Config {
+	return Config{v.db}
+}
+
+func (c Config) String(k string) (string, error) {
+	if c.db == nil {
+		return "", ErrLocked
+	}
+	return getConfig(c.db, k)
+}
+
+func (c Config) SetString(k string, v string) error {
+	if c.db == nil {
+		return ErrLocked
+	}
+	return setConfig(c.db, k, v)
+}
+
+func (c Config) KID(k string) (keys.ID, error) {
+	s, err := c.String(k)
+	if err != nil {
+		return "", err
+	}
+	kid, err := keys.ParseID(s)
+	if err != nil {
+		return "", err
+	}
+	return kid, nil
+}
+
+func (c Config) Bytes(k string) ([]byte, error) {
+	if c.db == nil {
+		return nil, ErrLocked
+	}
+	return getConfigBytes(c.db, k)
+}
+
+func (c Config) SetBytes(k string, v []byte) error {
+	if c.db == nil {
+		return ErrLocked
+	}
+	return setConfigBytes(c.db, k, v)
+}
+
+func (c Config) Set(k string, v string) error {
+	if c.db == nil {
+		return ErrLocked
+	}
+	return setConfig(c.db, k, v)
+}
 
 func setConfig(db *sqlx.DB, key string, value string) error {
 	if _, err := db.Exec("INSERT OR REPLACE INTO config (key, value) VALUES ($1, $2)", key, value); err != nil {
